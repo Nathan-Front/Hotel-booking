29fc2f169796d8c3e9952eb80af627f1ebc04e77
Nathan-Front <jonathantindungan3@gmail.com>
2026-02-20 10:44:53 +0900

    added more designs and functions to reserve html

--- script/reserve.js
+++ script/reserve.js
@@ -1,25 +1,60 @@
 
-function displayOtherRooms(){
-    const showMore = document.querySelector(".more-rooms");
-    const rooms = document.querySelector(".section-sub-wrapper");
-    showMore.addEventListener("click", ()=>{
-        if (rooms.classList.contains("show-other-rooms")) {
-            // collapse
-            rooms.style.height = rooms.scrollHeight + "px";
-            requestAnimationFrame(() => {
-                rooms.style.height = "450px";
-            });
-            rooms.classList.remove("show-other-rooms");
-        } else {
-            // expand
-            rooms.style.height = rooms.scrollHeight + "px";
+async function fetchNavbar(){
+  const res = await fetch("navigation.html");
+  const navHTML = await res.text();
+  const navContainer = document.querySelector("body");
+  navContainer.insertAdjacentHTML("afterbegin", navHTML);
+}
+fetchNavbar();
 
-            rooms.addEventListener("transitionend", function handler() {
-                rooms.style.height = "auto";
-                rooms.removeEventListener("transitionend", handler);
-            });
-             rooms.classList.toggle("show-other-rooms");
-        }
+async function fetchBottomSections() {
+  const body = document.body;
+
+  // Load footer first
+  const footerRes = await fetch("footer.html");
+  const footerHTML = await footerRes.text();
+  body.insertAdjacentHTML("beforeend", footerHTML);
+
+  // Then load alright
+  const alrightRes = await fetch("alright.html");
+  const alrightHTML = await alrightRes.text();
+  body.insertAdjacentHTML("beforeend", alrightHTML);
+
+  if(window.innerWidth > 540) return;
+    const res = await fetch("mobileNavigation.html");
+    const mobileNavHTML = await res.text();
+    const container = document.querySelector("body");
+    container.insertAdjacentHTML("beforeend", mobileNavHTML);
+}
+fetchBottomSections();
+
+function displayOtherRooms(){
+    const showMore = document.querySelectorAll(".more-rooms");
+    const moreRooms = document.querySelector(".more-rooms");
+    showMore.forEach(btn =>{
+        btn.addEventListener("click", ()=>{
+            const mainWrapper = btn.closest(".reserve-section").querySelector(".section-sub-wrapper");
+            if (mainWrapper.classList.contains("show-other-rooms") && btn.classList.contains("active") ) {
+                // collapse
+                mainWrapper.style.height = mainWrapper.scrollHeight + "px";
+                requestAnimationFrame(() => {
+                    mainWrapper.style.height = "450px";
+                });
+                mainWrapper.classList.remove("show-other-rooms");
+                btn.classList.remove("active");
+            } else {
+                // expand
+                mainWrapper.style.height = mainWrapper.scrollHeight + "px";
+
+                mainWrapper.addEventListener("transitionend", function handler() {
+                    mainWrapper.style.height = "auto";
+                    mainWrapper.removeEventListener("transitionend", handler);
+                });
+                 mainWrapper.classList.toggle("show-other-rooms");
+                 btn.classList.toggle("active");
+            }
+        });
     });
+
 }
 displayOtherRooms();
